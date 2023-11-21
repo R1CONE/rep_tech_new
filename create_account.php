d7e3eacb38c377889b5647655de2232e958bb1b8
R1CONE <116386177+R1CONE@users.noreply.github.com>
2023-11-21 08:33:39 +0100

    Update create_account.php

--- create_account.php
+++ create_account.php
@@ -1,42 +1,50 @@
 <!DOCTYPE html>
-<html lang="pl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logowanie</title>
+    <title>Registration</title>
     <style>
         body {
             font-family: Arial, sans-serif;
-            text-align: center;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        #registrationForm {
+            width: 300px;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
 
         h1 {
-            color: #333;
-        }
-
-        #loginForm {
-            width: 300px;
-            margin: 0 auto;
-            padding: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            background-color: #f7f7f7;
+            color: #007BFF;
         }
 
         label {
             display: block;
             text-align: left;
-            margin-bottom: 10px;
+            margin: 10px 0;
             font-weight: bold;
+            color: #333;
         }
 
         input[type="text"],
         input[type="password"] {
             width: 100%;
             padding: 10px;
-            margin-bottom: 20px;
+            margin-bottom: 15px;
             border: 1px solid #ccc;
             border-radius: 5px;
+            box-sizing: border-box;
         }
 
         input[type="submit"] {
@@ -54,36 +62,39 @@
 
         #hashResult {
             font-weight: bold;
+            color: #333;
+            margin-top: 10px;
         }
     </style>
 </head>
 <body>
-    <h1>Logowanie</h1>
-    <form id="loginForm" method="post">
+    <div id="registrationForm">
+        <h1>Registration</h1>
+        <form method="post">
 
-        <label for="username">Name:</label>
-        <input type="text" id="username" name="username" value="">
+            <label for="username">Name:</label>
+            <input type="text" id="username" name="username" placeholder="Enter your name">
 
-        <label for="surname">Surname:</label>
-        <input type="text" id="surname" name="surname" value="">
+            <label for="surname">Surname:</label>
+            <input type="text" id="surname" name="surname" placeholder="Enter your surname">
 
-        <label for="login">login:</label>
-        <input type="text" id="login" name="login" value="">
+            <label for="login">Login:</label>
+            <input type="text" id="login" name="login" placeholder="Choose a login">
 
-        <label for="email">email:</label>
-        <input type="text" id="email" name="email" value="">
+            <label for="email">Email:</label>
+            <input type="text" id="email" name="email" placeholder="Enter your email">
 
-        <label for="password">password:</label>
-        <input type="password" id="password" name="password" value="">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="Enter password">
 
-        <label for="telephone_number">telephone number:</label>
-        <input type="text" id="telephone_number" name="telephone_number" value="">
+            <label for="telephone_number">Phone Number:</label>
+            <input type="text" id="telephone_number" name="telephone_number" placeholder="Enter phone number">
+
+            <input type="submit" value="Register">
+            
+        </form>
         
-        <input type="submit" value="Zaloguj się" id="button-link">
-        
-    </form>
-    
-    <div id="hashResult"></div>
+        <div id="hashResult"></div>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -97,29 +108,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!empty($userLogin) && !empty($userPassword)) {
         $servername = "localhost";
         $username = "root";
-        $dbPassword = ""; // замените на пароль базы данных
+        $dbPassword = ""; // replace with your database password
         $database = "try_bd";
 
-        // Создайте подключение к базе данных
+        // Create a database connection
         $conn = new mysqli($servername, $username, $dbPassword, $database);
 
-        // Проверка подключения
+        // Check the connection
         if ($conn->connect_error) {
-            die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+            die("Connection error: " . $conn->connect_error);
         } else {
-            // Подготовленный запрос для вставки данных
+            // Prepared statement for inserting data
             $sql = "INSERT INTO accounts (login, password, name, surname, telefnum, email) VALUES (?, ?, ?, ?, ?, ?)";
             $stmt = $conn->prepare($sql);
             
-            // Привязываем параметры
+            // Bind parameters
             $stmt->bind_param("ssssss", $userLogin, $userPassword, $userName, $userSurname, $userTelephoneNumber, $userEmail);
             
-            // Выполняем запрос
+            // Execute the query
             $stmt->execute();
             
-            echo "Успешно зарегистрировано!";
+            echo "Successfully registered!";
             
-            // Закрываем подготовленное выражение и соединение
+            // Close the prepared statement and connection
             $stmt->close();
             $conn->close();
         }
